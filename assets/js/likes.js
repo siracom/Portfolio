12b6dc23d4ad2eb4f290cf6149ff8c567a352fb3
Sivakumar Panda <40688849+siracom@users.noreply.github.com>
2020-08-07 18:31:43 +0530

    Add files via upload

---
+++ assets/js/likes.js
@@ -0,0 +1,10 @@
+// var count_of_likes = 0;
+// document.readFile('assests/docs/likes.txt', (err, data) => { 
+//     if (err) throw err; 
+//     count_of_likes = parseInt(data);
+// }); 
+
+function clickCounter() {
+      $(".thumbs-up-bg").css("color", "#6f4a8e");
+      $(".thumbs-up-bg").fadeOut(5000);
+}
